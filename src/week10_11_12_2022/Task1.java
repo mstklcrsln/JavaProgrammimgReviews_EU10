57e5c95c14e5a8a8abef381eaf01382f4c2cf423
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-12 21:56:27 +0300

    last updates

--- src/week10_11_12_2022/Task1.java
+++ src/week10_11_12_2022/Task1.java
@@ -4,9 +4,36 @@ public class Task1 {
 
     public static void main(String[] args) {
 
+        String str = "(((W&e**_lco73me %T%o Co!@$,,<r>e <J>>>$@av^453a";
+        getSentence(str);
 
 
+    }
 
+    private static void getSentence(String str) {
+        String temp= "";
+        for (int i = 0; i <str.length() ; i++) {
+            char ch = str.charAt(i);
+           /* if (('a' <=ch && ch<='z') || ('A' <=ch && ch<='Z')) {
+                temp += ch;
+                //this is the old method
+            }
+             if we talk english letters we use isAlphbetic method
+            if we are talking other letters we can use isLetter method
+
+            */
+
+           // if (Character.isUpperCase(ch))        first solution
+             //   temp+=' ';
+
+
+            if (Character.isLetter(ch) ||ch== ' '){ // we put  ||ch== ' ' and put spaces
+                temp+= ch;
+            }
+
+
+        }
+        System.out.println("temp = " + temp); //temp = WelcomeToCoreJava
     }
 }
 /*
