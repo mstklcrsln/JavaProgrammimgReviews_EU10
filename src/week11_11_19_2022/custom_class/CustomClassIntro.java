ab7eaf2af8cd5869a0da5056b85f7a1cde4152ce
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-19 23:33:15 +0300

    week11_11_19_2022 Custom Classes

---
+++ src/week11_11_19_2022/custom_class/CustomClassIntro.java
@@ -0,0 +1,16 @@
+package week11_11_19_2022.custom_class;
+
+public class CustomClassIntro {
+    public static void main(String[] args) {
+        // how to create an object
+
+        // class name reference = new keyword constructor
+        SecondClass secondClass= new SecondClass();
+                                    //FirstClass(int a, String b, SecondClass secondClass)
+        FirstClass firstObject = new FirstClass(4,"Mesut", secondClass)
+
+
+
+
+    }
+}
--- src/week11_11_19_2022/custom_class/FirstClass.java
+++ src/week11_11_19_2022/custom_class/FirstClass.java
@@ -1,12 +1,16 @@
 package week11_11_19_2022.custom_class;
 
+import java.util.ArrayList;
+
 public class FirstClass {
 
     public int a;
     // int a;  // we skip public
      String b;
      SecondClass secondClass;
-        // shortcut for creating constructor
+    ArrayList<Integer> arrayList;
+
+    // shortcut for creating constructor
             //for windows: alt+insert
 
     public FirstClass(int a, String b, SecondClass secondClass) {
