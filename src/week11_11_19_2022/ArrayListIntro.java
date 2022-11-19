38ab6ade9316e03a84d1bec8f5ef060cd13f494d
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-19 20:13:52 +0300

    week11_11_19_2022

--- src/week11_11_19_2022/ArrayListIntro.java
+++ src/week11_11_19_2022/ArrayListIntro.java
@@ -27,7 +27,36 @@ public class ArrayListIntro {
 
         // to update one element
         cities.set(2,"Vienna"); // we changed paris with vienna
-        System.out.println(cities); //[London, Corum, Vienna, Yozgat, Rome]
+        System.out.println(cities); //[London, Corum, Vienna, Yozgat, Rome]  set method is returning element which you uptaded
+
+        // how to learn position of the index or index of element
+        System.out.println(cities.indexOf("Yozgat")); //3 the index num
+        System.out.println(cities.indexOf("Paris")); // we get -1, because there is no Paris
+        System.out.println(cities.indexOf("Yo")); // we get -1, because there is no "Yo" in the ArrayList
+
+        // last indexof
+
+        System.out.println(cities.lastIndexOf("Vienna")); //2
+        // indexOf and lastIndexOf methods gives the same index if the element is uniq
+
+        cities.add("Vienna");
+        System.out.println(cities.indexOf("Vienna"));           //2  start from beginning and returns teh first
+        System.out.println(cities.lastIndexOf("Vienna"));      //5 start from last and returns the first from last
+
+        // how to remove one element
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(1);
+        System.out.println(numbers); //[1, 1, 2, 3, 4, 5, 1]
+
+        System.out.println(numbers.indexOf(1)); // it gives 0 ==> first matching one
+        System.out.println(numbers.lastIndexOf(1)); // it gives 6 ==> last matching one
 
 
     }
