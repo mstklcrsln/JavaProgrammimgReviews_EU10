5e64784e7715d72b8668f858b2872daee27f1e0f
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-26 19:30:31 +0300

    week26_11_19_2022

--- src/week12_26_11_2022/student/SudentObject.java
+++ src/week12_26_11_2022/student/SudentObject.java
@@ -1,5 +1,7 @@
 package week12_26_11_2022.student;
 
+import java.util.Scanner;
+
 public class SudentObject {
     public static void main(String[] args) {
 
@@ -11,5 +13,12 @@ public class SudentObject {
 
         System.out.println(student2); //Student{name='Mesut', age=25, batchNumber=2022012515, school = Cydeo}
 
+        Student student4= new Student("Osman");
+        Scanner scan= new Scanner(System.in);
+        System.out.println("please enter " + student4.name +"'s age");
+        int age= scan.nextInt();
+        student4.age= age;
+        //we get the age from user and assign it to our 4th student
+        System.out.println(student4); //Student{name='Osman', age=25, batchNumber=0, school = Cydeo}
     }
 }
