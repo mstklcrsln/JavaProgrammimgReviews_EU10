7f8b812fd7b887135751a54a2371bc62f7053093
Mesut Kılıçarslan <mstklcrsln81@gmail.com>
2022-11-26 18:29:33 +0300

    week26_11_19_2022

---
+++ src/week12_26_11_2022/employee/Employee_Test.java
@@ -0,0 +1,11 @@
+package week12_26_11_2022.employee;
+
+public class Employee_Test {
+
+    Employee employee1 = new Employee("Adam", 1, 'M');
+
+        Employee employee2 = new Employee("Adam");
+        Employee Employee3 = new Employee("Adam",1);
+
+
+}
